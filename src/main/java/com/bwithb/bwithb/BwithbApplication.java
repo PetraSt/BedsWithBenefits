f0d48f86c6ced373e1c6bd9e95778b357f27a853
PetraSt <petra.stamou@gmail.com>
2025-01-15 11:22:59 +0100

    adding spring project

---
+++ src/main/java/com/bwithb/bwithb/BwithbApplication.java
@@ -0,0 +1,13 @@
+package com.bwithb.bwithb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BwithbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BwithbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bwithb/bwithb/BwithbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bwithb.bwithb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BwithbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
