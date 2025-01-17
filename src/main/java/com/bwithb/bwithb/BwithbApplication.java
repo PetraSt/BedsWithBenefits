ca6fee23d780f0055a99fa98940fa120ae715850
PetraSt <petra.stamou@gmail.com>
2025-01-17 11:58:54 +0100

    adding the first table and the connection to the db

--- src/main/java/com/bwithb/bwithb/BwithbApplication.java
+++ src/main/java/com/bwithb/bwithb/BwithbApplication.java
@@ -5,10 +5,12 @@ import java.util.Arrays;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EntityScan(basePackages = "com.bwithb.bwithb.entities")
 public class BwithbApplication {
 
 	public static void main(String[] args) {
